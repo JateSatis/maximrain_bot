2bb7f9b7721ef3ad3bdfdc12dfc2b9f7e5a1c96d
Maxim Danilov <maxdanilovdev@gmail.com>
2025-02-27 14:50:22 +0300

    Add JSON middleware, add logging for update message from telegram to handle it correctly

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,56 +7,21 @@ dotenv.config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
+const app = express();
+
+app.listen(3000, async () => {
+  console.log(`Server is up and running on port: ${3000}`);
+});
+
+// Parse JSON bodies
+app.use(express.json());
+
+// Parse URL-encoded bodies (if needed)
+app.use(express.urlencoded({ extended: true }));
+
 // URL для взаимодействия с API Telegram
 const API_URL = `https://api.telegram.org/bot${BOT_TOKEN}`;
 console.log(API_URL);
-
-// Переменная для отслеживания последнего обработанного обновления
-let lastUpdateId: number | null = null;
-
-// Функция для получения обновлений
-async function getUpdates() {
-  try {
-    // Отправляем запрос к методу getUpdates
-    const response = await axios.get(`${API_URL}/getUpdates`, {
-      params: {
-        offset: lastUpdateId ? lastUpdateId + 1 : undefined, // Начинаем с последнего обработанного ID + 1
-        timeout: 30, // Долгий опрос (до 30 секунд)
-      },
-    });
-
-    const updates = response.data.result;
-
-    if (updates.length > 0) {
-      console.log(`Получено ${updates.length} обновлений:`);
-
-      // Обрабатываем каждое обновление
-      for (const update of updates) {
-        processUpdate(update);
-        lastUpdateId = update.update_id; // Обновляем lastUpdateId
-      }
-    }
-  } catch (error) {
-    console.error(
-      "Ошибка при получении обновлений:",
-      error.response?.data || error.message
-    );
-  }
-}
-
-// Функция для обработки одного обновления
-function processUpdate(update: any) {
-  console.log(update);
-  if (update.message) {
-    const chatId = update.message.chat.id;
-    const text = update.message.text;
-
-    console.log(`Сообщение от пользователя: ${text}`);
-
-    // Отправляем ответное сообщение
-    sendMessage(chatId, `Вы сказали: ${text}`);
-  }
-}
 
 // Функция для отправки сообщения
 async function sendMessage(chatId: number, text: string) {
@@ -74,29 +39,10 @@ async function sendMessage(chatId: number, text: string) {
   }
 }
 
-// Главный цикл Long Polling
-async function startLongPolling() {
-  console.log("Начало Long Polling...");
-  while (true) {
-    await getUpdates();
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Ждём 1 секунду перед следующим запросом
-  }
-}
-
-// Запускаем Long Polling
-// startLongPolling();
-
-const app = express();
-
-app.listen(3000, async () => {
-  console.log(`Server is up and running on port: ${3000}`);
-});
-
 const mainRouter = Router();
 
 mainRouter.post("/webhook", async (req: Request, res: Response) => {
-  console.log("Получено сообщение");
-  res.status(200).json({ message: "Получено сообщение" });
+  console.log(req.body);
   return;
 });
 
